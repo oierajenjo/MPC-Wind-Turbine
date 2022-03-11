b71a63188d9026341cbe0c3419e0ceb54c8c886c
oierajenjo <oierajenjo@opendeusto.es>
2022-03-11 14:51:20 +0100

    G matrix MPC

--- MPC/MPCdefinition.m
+++ MPC/MPCdefinition.m
@@ -1,5 +1,34 @@
 %Define the MPC object with Yalmip:
- 
+clear all
+clc
+variables_IPC
+% Constraints limit values
+U_c.theta_min = 0;
+U_c.theta_max = pi/2;
+U_c.Tg_min = 1;
+U_c.Tg_max = 2.159e7;
+
+dU_c.theta_dot = deg2rad(9); % Pitch angle max and min angular speed
+
+Z_c.omega_r_min = convangvel(5,'rpm', 'rad/s');
+Z_c.omega_r_max = - convangvel(10,'rpm', 'rad/s');
+Z_c.xtdd_min = 0; 
+Z_c.xtdd_max = -0;
+Z_c.ytdd_min = 0;
+Z_c.ytdd_max = -0;
+Z_c.My_min1 = B.m*B.l*B.xdd_min; Z_c.My_max1 = -B.m*B.l*B.xdd_max;
+Z_c.My_min2 = B.m*B.l*B.xdd_min; Z_c.My_max2 = -B.m*B.l*B.xdd_max;
+Z_c.My_min3 = B.m*B.l*B.xdd_min; Z_c.My_max3 = -B.m*B.l*B.xdd_max;
+Z_c.Mx_min1 = B.m*B.l*B.ydd_min; Z_c.Mx_max1 = -B.m*B.l*B.ydd_max;
+Z_c.Mx_min2 = B.m*B.l*B.ydd_min; Z_c.Mx_max2 = -B.m*B.l*B.ydd_max;
+Z_c.Mx_min3 = B.m*B.l*B.ydd_min; Z_c.Mx_max3 = -B.m*B.l*B.ydd_max;
+Z_c.Pe_min = D.eta*U_c.Tg_min*Z_c.omega_r_min;
+Z_c.Pe_max = -D.eta*U_c.Tg_max*Z_c.omega_r_max;
+Z_c.vr_min = 4;
+Z_c.vr_max = -25;
+Z_c.azim_min = 0;
+Z_c.azim_max = -0;
+
 % Ts = Ts; % Sample time for the MPC controller
 Ts = 0.05;
 Hp = 10; % Prediction Horizon
@@ -79,7 +108,7 @@ Rcal = blkdiag(Rcal{:});
 % Rcal_del = repmat({R_del}, 1, Hu);
 % Rcal_del = blkdiag(Rcal_del{:});
 
-Epsilon = Tau - Psi*P0 -Upsilon*Uprev;
+Epsilon = Tau - Psi*P0 - Upsilon*Uprev;
 
 P = Acal*P0 + Bcal_u*Uprev + Bcal_du*deltaU; % P: P*: Optimal states in prediction window
 
@@ -113,7 +142,8 @@ Cost = Epsilon'*Qcal*Epsilon - deltaU'*Gcal + deltaU'*Hcal*deltaU;
 f = [-1; 1];
 f_rep = repmat({f}, 1, Uk*Hu);
 F_L = blkdiag(f_rep{:});
-u_cons = [0; -pi/2; 0; -pi/2; 0; -pi/2; 1; 2.159e7];
+u_cons = [U_c.theta_min; -U_c.theta_max; U_c.theta_min; -U_c.theta_max; 
+        U_c.theta_min; -U_c.theta_max; U_c.Tg_min; -U_c.Tg_max];
 U_L = u_cons;
 for i=1:Hu-1
     U_L = [U_L; u_cons]; 
@@ -123,26 +153,35 @@ F = [F_L U_L];
 % Actuator Slew Rate Constraints
 e = [-1; 1];
 e_rep = repmat({e}, 1, Uk-1);
+e_rep{end+1} = zeros(2,1);
 e_L = blkdiag(e_rep{:});
-e_L = [e_L zeros((Uk-1)*2,1); zeros(2,Uk)];
 
 e_L = repmat({e_L}, 1, Hu);
 E_L = blkdiag(e_L{:});
-du_cons = [-deg2rad(9)*ones(6,1); 0; 0];
+du_cons = [-dU_c.theta_dot*ones(6,1); zeros(2,1)];
 dU_L = du_cons;
 for i=1:Hu-1
    dU_L = [dU_L; du_cons]; 
 end
 E = [E_L dU_L];
 
-% g = [0 -1];
-% g_rep = repmat({g}, [1 Hp]);
-% G_L = blkdiag(g_rep{:});
-% GL = [G_L ones(Hp,1)*3*0.95];%(y_constraint+0.15/2)*0.99];
-%     
-% max_v = 0.22/sqrt(Lk);
+% Actuator Range Constraints
+g = [-1; 1];
+g_rep = repmat({g}, 1, Yk-1);
+g_rep{end+1} = [-1;0];
+g_L = blkdiag(g_rep{:});
 
-Constraints = [F*[U;1]<=0; E*[deltaU;1]<=0];
+g_L = repmat({g_L}, 1, Hu);
+G_L = blkdiag(g_L{:});
+z_cons = cell2mat(struct2cell(Z_c));
+Z_L = z_cons;
+for i=1:Hu-1
+    Z_L = [Z_L; z_cons]; 
+end
+G = [G_L Z_L];
+Zcal = Psi*P0 + Upsilon*Uprev + Theta*deltaU;
+
+Constraints = [F*[U;1]<=0; E*[deltaU;1]<=0; G*[Zcal;1]<=0];
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % PLACE YOUR CODE HERE (END)
