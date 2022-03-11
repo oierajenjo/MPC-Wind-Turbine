f2a5c846d218538e59876b7d3c3ff1ecd7189237
oierajenjo <oierajenjo@opendeusto.es>
2022-03-11 14:52:04 +0100

    IPC minor changes

--- Bladed/variables_IPC.m
+++ Bladed/variables_IPC.m
@@ -16,20 +16,24 @@ B.kx = (2*pi*B.fx)^2*B.m; % Blade stiffness x direction
 B.cy = B.d*2*B.m*2*pi*B.fy; % Blade damping y direction
 B.ky = (2*pi*B.fy)^2*B.m; % Blade stiffness y direction
 B.l = 117.1836; % Blade length
+B.B = 3; % Blade amount
+B.xdd_min = 0;
+B.xdd_max = 0;
+B.ydd_min = 0;
+B.ydd_max = 0;
 
 %% Tower model constants
-To.m = 2475680; % Tower mass
-% T.m = 1389678; % Nacelle mass
+To.m = 2475680-B.m*B.B; % Tower mass
 To.d = 0.005; % Tower damping ratio
 To.f = 0.18; % Tower freq. flapwise
-To.c = To.d*2*To.m*2*pi*To.f;
+To.c = To.d*2*To.m*2*pi*To.f; % Tower damping
 To.k = (2*pi*To.f)^2*To.m; % Tower stiffness
 To.h = 144.582; % Tower height
 To.r_top = 3.25; % Tower top radius
 To.r_base = 5; % Tower base radius
 To.H = To.h + 4.34799; % Hub height
 To.r = (To.r_top-To.r_base)*(To.H-B.l)/To.H + To.r_base; % Tower radius
-To.xh = 10.93; % Tower overhang
+To.xh = 10.93; % Hub overhang
 
 %% Aerodynamic model constants
 Ae.rho = 1.225; % Density of the air
@@ -82,14 +86,22 @@ tg_ref = data.Data(:,20); % Generator Torque
 
 u = [theta_ref tg_ref]';
 
+%% Disturbances
+% vm = data.Data(:,59); % Wind mean speed
+vr = data.Data(:,54); % Wind speed
+Fy = sum([data.Data(:,66) data.Data(:,74) data.Data(:,82)], 2); % Fy in the principal axis
+wr = data.Data(:,10); % Rotor speed
+% d_b = vr';
+% d_b = [vr wr Fy]';
+
 %% Measurements
 omega_r = data.Data(:,10); % Rotor speed
 xt_ddot = data.Data(:,236); % Tower fore-aft acceleration
 yt_ddot = data.Data(:,237); % Tower edgewise acceleration
-Mx = [data.Data(:,111) data.Data(:,119) data.Data(:,127)];
-My = [data.Data(:,112) data.Data(:,120) data.Data(:,128)];
-% Mx = [data.Data(:,61) data.Data(:,69) data.Data(:,77)]; % Mx in the principal axis
-% My = [data.Data(:,62) data.Data(:,70) data.Data(:,78)]; % My in the principal axis
+% Mx = [data.Data(:,111) data.Data(:,119) data.Data(:,127)];
+% My = [data.Data(:,112) data.Data(:,120) data.Data(:,128)];
+Mx = [data.Data(:,61) data.Data(:,69) data.Data(:,77)]; % Mx in the principal axis
+My = [data.Data(:,62) data.Data(:,70) data.Data(:,78)]; % My in the principal axis
 Pe = data.Data(:,28);
 vr = data.Data(:,26); % Wind speed magnitud at the hub
 psi = data.Data(:,11);
@@ -97,24 +109,27 @@ psi = data.Data(:,11);
 y_me = [omega_r xt_ddot yt_ddot My Mx Pe vr psi]';
 
 %% Initial state vector
-xt = data.Data(1,224);
-xt_dot = data.Data(1,230);
-yt = data.Data(1,225);
+xt_dot = -data.Data(1,230);
+xt = -data.Data(1,224);
 yt_dot = data.Data(1,231);
+yt = data.Data(1,225);
+
 xb = [data.Data(1,85) data.Data(1,91) data.Data(1,97)];
 xb_dot = zeros(1,3);
 yb = [data.Data(1,86) data.Data(1,92) data.Data(1,98)];
 yb_dot = zeros(1,3);
+
 theta = theta_ref(1,:);
 theta_dot = data.Data(1,37:39);
+tg = tg_ref(1);
 vt = 0;
 vm = data.Data(1,59);
 
 x_i = [omega_r(1) xt xt_dot yt yt_dot xb xb_dot yb yb_dot theta theta_dot...
-    tg_ref(1) vt vm psi(1)]';
+    tg vt vm psi(1)]';
 
 N = data.Channels.Scans; % Number of time steps for filter
-clearvars -except D To B Ae Ac M Ts ti W w_p x_i y_me u N % a sigma_m sigma_t
+clearvars -except D To B Ae Ac M Ts ti W w_p x_i y_me u N data% a sigma_m sigma_t
 
 load('performancemap_data.mat')
 %% Plotting variables
--- KalmanFilter/IPC/kalman_filter.m
+++ KalmanFilter/IPC/kalman_filter.m
@@ -27,40 +27,50 @@ wm(1) = lambda/(lambda+Lk);
 wc(1) = lambda/(lambda+Lk) + 1 - alpha^2 + beta;
 
 % Step 2: Define noise assumptions
-wp = @(x) x(26)*pi/(2*W.L);
-a = @(x) 1-wp(x)*Ts; % Euler
-% a = @(x) exp(-(x(5)*pi/(2*L))*Ts); % Zero Order Hold
-ve = @(x) x(26) + x(25);
-vei = @(x,i) x(26)*(To.r^2*(Ae.Rr^2*(sin(x(27)+2*pi*(i-1)/3))^2-To.xh^2)/(To.xh^2+Ae.Rr^2*(sin(x(27)+2*pi*(i-1)/3))^2)^2 +...
-    ((Ae.Rr*cos(x(27)+2*pi*(i-1)/3)+To.H)/To.H)^W.alpha) + x(25);
+w_p = @(x) x(26)*pi/(2*W.L);
 
-Tr = @(x,u) 0.5*Ae.rho*Ae.Ar*(ve(x)-x(3))^3*cp_ct(x(1)*Ae.Rr/(ve(x)-x(3)),mean(x(18:20)),cp_l,lambdaVec,pitchVec)/x(1);
-Fri = @(x,u,i) 0.5*Ae.rho*Ae.Ar*(vei(x,i)-x(3)-x(9+i-1))^2*cp_ct(x(1)*Ae.Rr/(vei(x,i)-x(3)),x(18+i-1),ct_l,lambdaVec,pitchVec)/3; % Thrust coefficient
+ve = @(x) x(26) + x(25);
+vr = @(x) ve(x) - x(3);
+
+vei = @(x,i) x(26)*(To.r^2*(Ae.Rr^2*(sin(wrapTo2Pi(x(27)+2*pi*i/3)))^2-To.xh^2)/(To.xh^2+Ae.Rr^2*(sin(wrapTo2Pi(x(27)+2*pi*i/3)))^2)^2 +...
+    ((Ae.Rr*cos(wrapTo2Pi(x(27)+2*pi*i/3))+To.H)/To.H)^W.alpha) + x(25);
+vri = @(x,i) vei(x,i) - x(3);
+
+lamb = @(x) (x(1)*Ae.Rr-sum(x(15:17)))/(vr(x));
+lambi = @(x,i) (x(1)*Ae.Rr-x(15+i))/(vri(x,i)-x(9+i));
+
+cp = @(x) cp_ct(lamb(x),mean(x(18:20)),cp_l,lambdaVec,pitchVec);
+cpi = @(x,i) cp_ct(lambi(x,i),x(18+i),cp_l,lambdaVec,pitchVec)/B.B;
+ct = @(x,i) cp_ct(lambi(x,i),x(18+i),ct_l,lambdaVec,pitchVec);
+
+Tr = @(x) 0.5*Ae.rho*Ae.Ar*(vr(x))^3*cp(x)/x(1);
+Fxi = @(x,i) 0.5*Ae.rho*Ae.Ar*(vri(x,i)-x(9+i))^2*ct(x,i); % Thrust coefficient
+Fyi = @(x,i) (0.5*Ae.rho*Ae.Ar*(vri(x,i))^3*cpi(x,i)/x(1))/(2*Ae.Rr/3);
 
 %% Drive train
-f1 = @(x,u) ((1-D.mu)*Tr(x,u) - x(24))/(D.Jr+D.Jg);
+f1 = @(x) (1-D.mu)*Tr(x)/(D.Jr+D.Jg) - x(24)/(D.Jr+D.Jg);
 
 %% Tower
 f2 = @(x) x(3); % Tower foreafter velocity
-f3 = @(x,u) (B.kx*(sum(x(6:8)-x(2))) + B.cx*(sum(x(9:11)-x(3))) - To.k*x(2) - To.c*x(3))/To.m; % Tower foreafter acceleration
+f3 = @(x) -(B.B*B.kx + To.k)*x(2)/To.m - (B.B*B.cx + To.c)*x(3)/To.m + B.kx*sum(x(6:8))/To.m + B.cx*sum(x(9:11))/To.m; % Tower foreafter acceleration
 
 f4 = @(x) x(5); % Tower edgewise velocity
-f5 = @(x,u) (3*x(24)/(2*To.H) + B.ky*(sum(x(12:14)-x(4))) + B.cy*(sum(x(15:17)-x(5))) - To.k*x(4) - To.c*x(5))/To.m; % Tower edgewise acceleration
+f5 = @(x) 3*x(24)/(2*To.H*To.m) - (B.B*B.ky + To.k)*x(4)/To.m - (B.B*B.cy + To.c)*x(5)/To.m + B.ky*sum(x(12:14))/To.m + B.cy*sum(x(15:17))/To.m ; % Tower edgewise acceleration
 
 %% Blades
 f6 = @(x) x(9); % Blade 1 foreafter velocity
 f7 = @(x) x(10); % Blade 2 foreafter velocity
 f8 = @(x) x(11); % Blade 3 foreafter velocity
-f9 = @(x,u) (Fri(x,u,1) - B.kx*(x(6)-x(2)) - B.cx*(x(9)-x(3)))/B.m; % Blade 1 foreafter acceleration
-f10 = @(x,u) (Fri(x,u,2) - B.kx*(x(7)-x(2)) - B.cx*(x(10)-x(3)))/B.m; % Blade 2 foreafter acceleration
-f11 = @(x,u) (Fri(x,u,3) - B.kx*(x(8)-x(2)) - B.cx*(x(11)-x(3)))/B.m; % Blade 3 foreafter acceleration
+f9 = @(x) Fxi(x,0)/B.B*B.m + B.kx*x(2)/B.m + B.cx*x(3)/B.m - B.kx*x(6)/B.m - B.cx*x(9)/B.m; % Blade 1 foreafter acceleration
+f10 = @(x) Fxi(x,1)/B.B*B.m + B.kx*x(2)/B.m + B.cx*x(3)/B.m - B.kx*x(7)/B.m - B.cx*x(10)/B.m; % Blade 2 foreafter acceleration
+f11 = @(x) Fxi(x,2)/B.B*B.m + B.kx*x(2)/B.m + B.cx*x(3)/B.m - B.kx*x(8)/B.m - B.cx*x(11)/B.m; % Blade 3 foreafter acceleration
 
 f12 = @(x) x(15); % Blade 1 edgewise velocity
 f13 = @(x) x(16); % Blade 2 edgewise velocity
 f14 = @(x) x(17); % Blade 3 edgewise velocity
-f15 = @(x,u) (- B.ky*(x(12)-x(4)) - B.cy*(x(15)-x(5)))/B.m; % Blade 1 edgewise acceleration
-f16 = @(x,u) (- B.ky*(x(13)-x(4)) - B.cy*(x(16)-x(5)))/B.m; % Blade 2 edgewise acceleration
-f17 = @(x,u) (- B.ky*(x(14)-x(4)) - B.cy*(x(17)-x(5)))/B.m; % Blade 3 edgewise acceleration
+f15 = @(x) Fyi(x,0)/B.m + B.ky*x(4)/B.m + B.cy*x(5)/B.m - B.ky*x(12)/B.m - B.cy*x(15)/B.m; % Blade 1 edgewise acceleration
+f16 = @(x) Fyi(x,1)/B.m + B.ky*x(4)/B.m + B.cy*x(5)/B.m - B.ky*x(13)/B.m - B.cy*x(16)/B.m; % Blade 2 edgewise acceleration
+f17 = @(x) Fyi(x,2)/B.m + B.ky*x(4)/B.m + B.cy*x(5)/B.m - B.ky*x(14)/B.m - B.cy*x(17)/B.m; % Blade 3 edgewise acceleration
 
 %% Actuators
 f18 = @(x) x(21); % Pitch 1 velocity
@@ -73,25 +83,28 @@ f23 = @(x,u) Ac.omega^2*u(3) - 2*Ac.omega*Ac.xi*x(23) - Ac.omega^2*x(20); % Pitc
 f24 = @(x,u) (u(4)-x(24))/Ac.tau; % Torque change in time
 
 %% Wind
-f25 = @(x) -x(26)*pi*x(25)/(2*W.L); % Wind turbulence acceleration
+f25 = @(x) -w_p(x)*x(25); % Wind turbulence acceleration
 f26 = 0; % Mean wind acceleration
 
 %% Azimuth
 f27 = @(x) x(1); % Azimuth velocity
 
-f = @(x,u) x + Ts*[f1(x,u); f2(x); f3(x,u); f4(x); f5(x,u); f6(x); f7(x);...
-    f8(x); f9(x,u); f10(x,u); f11(x,u); f12(x); f13(x); f14(x); f15(x,u);...
-    f16(x,u); f17(x,u); f18(x); f19(x); f20(x); f21(x,u); f22(x,u);...
+f = @(x,u) x + Ts*[f1(x); f2(x); f3(x); f4(x); f5(x); f6(x); f7(x);...
+    f8(x); f9(x); f10(x); f11(x); f12(x); f13(x); f14(x); f15(x);...
+    f16(x); f17(x); f18(x); f19(x); f20(x); f21(x,u); f22(x,u);...
     f23(x,u); f24(x,u); f25(x); f26; f27(x)]; % Nonlinear prediction
 
 % h = @(x) (x);
-h = @(x,u) [x(1); f3(x,u); f5(x,u); B.l*B.m*f9(x,u); B.l*B.m*f10(x,u);...
-    B.l*B.m*f11(x,u); B.l*B.m*f15(x,u); B.l*B.m*f16(x,u); B.l*B.m*f17(x,u);...
+h = @(x) [x(1); f3(x); f5(x); B.l*B.m*f9(x); B.l*B.m*f10(x);...
+    B.l*B.m*f11(x); B.l*B.m*f15(x); B.l*B.m*f16(x); B.l*B.m*f17(x);...
     D.eta*x(24)*x(1); ve(x); x(27)];
 
+
+a = @(x) 1-w_p(x)*Ts; % Euler
+% a = @(x) exp(-(x(5)*pi/(2*L))*Ts); % Zero Order Hold
 sigma_t = @(x) ti*x(26)*sqrt((1-a(x)^2)/(1-a(x))^2);
 sigma_m = sqrt(Ts*W.q);
-Q = @(x) diag([zeros(Lk-3,1); sigma_t(x)^2*wp(x)^2; sigma_m^2; 0]); % Covariance matrix of the process noise
+Q = @(x) diag([zeros(Lk-3,1); sigma_t(x)^2*w_p(x)^2; sigma_m^2; 0]); % Covariance matrix of the process noise
 
 temp = [M.sigma_enc; M.sigma_acc; M.sigma_acc; M.sigma_root; M.sigma_root;...
     M.sigma_root; M.sigma_root; M.sigma_root; M.sigma_root; M.sigma_pow;...
@@ -119,52 +132,48 @@ y = zeros(Yk, N); % Initialize size of output vector for all k
 % Generate the true state values
 for k = 2:N
     xt(:,k) = f(xt(:,k-1),u(:,k-1)) + Ts*n(xt(:,k-1));
-    y(:,k-1) = h(xt(:,k-1),u(:,k-1)) + v(:,k-1);
+    y(:,k-1) = h(xt(:,k-1)) + v(:,k-1);
 end
 for k=1:N
-    p(k) = ve(xt(:,k))-xt(3,k);
-    p1(k) = vei(xt(:,k),1);
-    p2(k) = vei(xt(:,k),2);
-    p3(k) = vei(xt(:,k),3);
-    tr(k) = (1-D.mu)*Tr(xt(:,k),u(:,k));
+    p(k) = vr(xt(:,k));
+    p1(k) = vri(xt(:,k),1);
+    p2(k) = vri(xt(:,k),2);
+    p3(k) = vri(xt(:,k),3);
+    tr(k) = (1-D.mu)*Tr(xt(:,k));
 end
 figure
-plot(xt(1,1:5000));
+plot(xt(1,1:500));
 title("wr")
 figure
-plot(xt(2,1:5000));
+plot(xt(2,1:500));
 title("xt")
-figure
-plot(xt(3,1:5000));
-title("xtdot")
-figure
-plot(xt(6,1:5000));
-title("xb1")
 % figure
-% plot(xt(4,1:500));
-% title("yt")
+% plot(xt(3,1:500));
+% title("xtdot")
+figure
+plot(xt(6,1:500));
+title("xb1")
+figure
+plot(xt(4,1:500));
+title("yt")
 % figure
 % plot(xt(5,1:500));
 % title("ytdot")
-% figure
-% plot(xt(12,1:500));
-% title("yb1")
+figure
+plot(xt(12,1:500));
+title("yb1")
 % figure
 % plot(xt(24,:));
 % title("Tg")
 figure
-plot(p(1:5000))
+plot(p(1:500))
 title("vr")
 figure
-plot(1:5000,tr(1:5000),1:5000,xt(24,1:5000))
-title("tr & tg")
-legend("tr", "tg")
+plot(1:500,tr(1:500),1:500,xt(24,1:500))
+title("Tr & Tg")
+legend("Tr", "Tg")
 figure
-plot(tr(1:5000)-xt(24,1:5000))
-title("Tr-Tg")
-legend("err")
-figure
-plot(1:N,p1(:),1:N,p2(:),1:N,p3(:))
+plot(1:500,p1(1:500),1:500,p2(1:500),1:500,p3(1:500))
 title("ve1, ve2, ve3")
 
 % %% Initialize and run EKF for comparison
@@ -220,7 +229,7 @@ for k = 2:N
     % obtaining the acceleration
     psi_m = zeros(Yk,n_sigma_p);
     for j=1:n_sigma_p
-        psi_m(:,j) = h(chi_m(:,j),u(:,k-1));
+        psi_m(:,j) = h(chi_m(:,j));
     end
     y_m = psi_m*wm; % Calculate mean of predicted output
     
