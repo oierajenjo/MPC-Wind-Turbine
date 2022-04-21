82e4961b80e45a50f4c8b625c55e7f4c330b5cbe
alvaromgp <alvaromgp@gmail.com>
2022-04-21 09:29:38 +0200

    Update MPCconstants.m

--- Main/MPCconstants.m
+++ Main/MPCconstants.m
@@ -12,8 +12,8 @@ b4 = (B.B*b2 + To.c/To.m);
 c1 = 3/(2*To.H*To.m);
 c2 = B.ky/To.m;
 c3 = B.cy/To.m;
-c4 = (B.B*c1 + To.k/To.m);
-c5 = (B.B*c2 + To.c/To.m);
+c4 = (B.B*c2 + To.k/To.m);
+c5 = (B.B*c3 + To.c/To.m);
 
 % xb1_dot
 ws_ts = @(x,i) (To.r^2*(Ae.Rr^2*(sin(x(27)+2*pi*i/3))^2-To.xh^2)/(To.xh^2+Ae.Rr^2*(sin(x(27)+2*pi*i/3))^2)^2 +...
@@ -22,14 +22,28 @@ vri_eq = @(x,i) x(26)*ws_ts(x,i) + x(25) - x(3)- x(9+i);
 lambi_eq = @(x,i) (x(1)*Ae.Rr-x(15+i))/(vri_eq(x,i));
 cpi_eq = @(x,i) cp_ct(lambi_eq(x,i),x(18+i),cp_l,lambdaVec,pitchVec)/B.B;
 cti_eq = @(x,i) cp_ct(lambi_eq(x,i),x(18+i),ct_l,lambdaVec,pitchVec)/B.B;
-dct_dlamb = 1; % Gradient
-dct_dpitch = 1; % Gradient
 
+<<<<<<< Updated upstream
 dvri_dazim = @(x,i) x(26)*(To.r^2*((2*Ae.Rr^2*cos(x(27)+2*pi*i/3)*sin(x(27)+2*pi*i/3))...
     *(Ae.Rr^2*(sin(x(27)+2*pi*i/3))^2 + To.xh^2)^2 - (Ae.Rr^2*(sin(x(27)+2*pi*i/3))^2 - To.xh^2)...
     *2*(2*Ae.Rr^2*cos(x(27)+2*pi*i/3)*sin(x(27)+2*pi*i/3))*(Ae.Rr^2*(sin(x(27)+2*pi*i/3))^2 + To.xh^2))...
     /(Ae.Rr^2*(sin(x(27)+2*pi*i/3))^2 + To.xh^2)^4 ...
     + W.alpha*(-Ae.Rr*sin(x(27)+2*pi*i/3)/To.H)*((Ae.Rr*sin(x(27)+2*pi*i/3)+To.H)/To.H)^(W.alpha-1));
+=======
+% Gradients (dCt_dLambda, dCt_dPitch, dCp_dLambda, dCp_dPitch)
+[dCp_dTheta_vec, dCp_dLambda_vec] = gradient(cp_l);
+[dCt_dTheta_vec, dCt_dLambda_vec] = gradient(ct_l);
+dCp_dTheta_eq = @(x,i) cp_ct_grad(lambi_eq(x,i),x(18+i),dCp_dTheta_vec,lambdaVec,pitchVec)/B.B;
+dCp_dLambda_eq = @(x,i) cp_ct_grad(lambi_eq(x,i),x(18+i),dCp_dLambda_vec,lambdaVec,pitchVec)/B.B;
+dCt_dTheta_eq = @(x,i) cp_ct_grad(lambi_eq(x,i),x(18+i),dCt_dTheta_vec,lambdaVec,pitchVec)/B.B;
+dCt_dLambda_eq = @(x,i) cp_ct_grad(lambi_eq(x,i),x(18+i),dCt_dLambda_vec,lambdaVec,pitchVec)/B.B;
+
+dvri_dazim = @(x,i) x(26)*(To.r^2*(Ae.Rr^2*(sin(x(27)+2*pi*i/3))^2 + To.xh^2)^2*...
+    (2*Ae.Rr^2*sin(x(27)+2*pi*i/3)*cos(x(27)+2*pi*i/3)))/(Ae.Rr^2*(sin(x(27)+2*pi*i/3))^2 + To.xh^2)^4 ...
+    - (To.r^2*(Ae.Rr^2*(sin(x(27)+2*pi*i/3))^2 - To.xh^2)*...
+    2*(2*Ae.Rr^2*sin(x(27)+2*pi*i/3)*cos(x(27)+2*pi*i/3))*(Ae.Rr^2*(sin(x(27)+2*pi*i/3))^2 + To.xh^2))/(Ae.Rr^2*(sin(x(27)+2*pi*i/3))^2 + To.xh^2)^4 ...
+    + W.alpha*(-Ae.Rr*sin(x(27)+2*pi*i/3)/To.H)*((Ae.Rr*cos(x(27)+2*pi*i/3)+To.H)/To.H)^(W.alpha-1);
+>>>>>>> Stashed changes
 
 dlamb_dvt = @(x,i) -(x(1)*Ae.Rr-x(15+i))/vri_eq(x,i)^2;
 dlamb_dxdott = @(x,i) (x(1)*Ae.Rr-x(15+i))/vri_eq(x,i)^2;
@@ -40,8 +54,9 @@ dlamb_dazim = @(x,i) -(x(1)*Ae.Rr-x(15+i))*dvri_dazim(x,i)/vri_eq(x,i)^2;
 dlamb_dwr = @(x,i) Ae.Rr/vri_eq(x,i);
 
 d1 = B.kx/B.m; % dx_bi
-d2 = @(x,i) Ae.rho*Ae.Ar*(dct_dlamb*dlamb_dxdotbi(x,i)*vri_eq(x,i)^2 - 2*cti_eq(x,i)*vri_eq(x,i) - B.cx/B.m)/(6*B.m); % dxdot_bi
+d2 = @(x,i) Ae.rho*Ae.Ar*(dCt_dLambda_eq(x,i)*dlamb_dxdotbi(x,i)*vri_eq(x,i)^2 - 2*cti_eq(x,i)*vri_eq(x,i) - B.cx/B.m)/(6*B.m); % dxdot_bi
 d3 = d1; % dx_t
+<<<<<<< Updated upstream
 d4 = @(x,i) Ae.rho*Ae.Ar*(dct_dlamb*dlamb_dxdott(x,i)*vri_eq(x,i)^2 - 2*cti_eq(x,i)*vri_eq(x,i) + B.cx/B.m)/(6*B.m); % dxdot_t
 d5 = @(x,i) Ae.rho*Ae.Ar*(dct_dlamb*dlamb_dvm(x,i)*vri_eq(x,i)^2 + 2*cti_eq(x,i)*ws_ts(x,i)*vri_eq(x,i))/(6*B.m); % dv_m
 d6 = @(x,i) Ae.rho*Ae.Ar*(dct_dlamb*dlamb_dvt(x,i)*vri_eq(x,i)^2 + 2*cti_eq(x,i)*vri_eq(x,i))/(6*B.m); % dv_t
@@ -49,26 +64,36 @@ d7 = @(x,i) Ae.rho*Ae.Ar*vri_eq(x,i)^2*dct_dpitch/(6*B.m); % dpitch
 d8 = @(x,i) Ae.rho*Ae.Ar*vri_eq(x,i)^2*dct_dlamb*dlamb_dydotbi(x,i)/(6*B.m); % dydot_bi
 d9 = @(x,i) Ae.rho*Ae.Ar*(2*cti_eq(x,i)*dvri_dazim(x,i)*vri_eq(x,i) + dct_dlamb*dlamb_dazim(x,i)*vri_eq(x,i)^2)/(6*B.m); % dazim
 d10 = @(x,i) Ae.rho*Ae.Ar*vri_eq(x,i)^2*dct_dlamb*dlamb_dwr(x,i)/(6*B.m); % dw_r
+=======
+d4 = @(x,i) Ae.rho*Ae.Ar*(dCt_dLambda_eq(x,i)*dlamb_dxdott(x,i)*vri_eq(x,i)^2 - 2*cti_eq(x,i)*vri_eq(x,i) - B.cx/B.m)/(6*B.m); % dxdot_t
+d5 = @(x,i) Ae.rho*Ae.Ar*(dCt_dLambda_eq(x,i)*dlamb_dvm(x,i)*vri_eq(x,i)^2 + 2*cti_eq(x,i)*ws_ts(x,i)*vri_eq(x,i))/(6*B.m); % dv_m
+d6 = @(x,i) Ae.rho*Ae.Ar*(dCt_dLambda_eq(x,i)*dlamb_dvt(x,i)*vri_eq(x,i)^2 + 2*cti_eq(x,i)*vri_eq(x,i))/(6*B.m); % dv_t
+d7 = @(x,i) Ae.rho*Ae.Ar*vri_eq(x,i)^2*dCt_dTheta_eq(x,i)/(6*B.m); % dpitch
+d8 = @(x,i) Ae.rho*Ae.Ar*vri_eq(x,i)^2*dCt_dLambda_eq(x,i)*dlamb_dydotbi(x,i)/(6*B.m); % dydot_bi
+d9 = @(x,i) Ae.rho*Ae.Ar*(2*cti_eq(x,i)*dvri_dazim(x,i)*vri_eq(x,i) + dCt_dLambda_eq(x,i)*dlamb_dazim(x,i)*vri_eq(x,i)^2)/(6*B.m); % dazim
+d10 = @(x,i) Ae.rho*Ae.Ar*vri_eq(x,i)^2*dCt_dLambda_eq(x,i)*dlamb_dwr(x,i)/(6*B.m); % dw_r
+>>>>>>> Stashed changes
 
 % yb1_dot
-dcp_dlamb = 1; % Gradient
-dcp_dpitch = 1; % Gradient
-
 e1 = B.ky/B.m; % dy_bi
+<<<<<<< Updated upstream
 e2 = @(x,i) Ae.rho*Ae.Ar*vri_eq(x,i)^3*(dcp_dlamb*dlamb_dydotbi(x,i)/(4*x(1)*B.l) + B.cy/B.m); % dydot_bi
+=======
+e2 = @(x,i) Ae.rho*Ae.Ar*vri_eq(x,i)^3*(dCp_dLambda_eq(x,i)*dlamb_dydotbi(x,i) + B.cy/B.m)/(4*x(1)*B.l); % dydot_bi
+>>>>>>> Stashed changes
 e3 = e1; % dy_t
 e4 = B.cy/B.m; % dydot_t
-e5 = @(x,i) Ae.rho*Ae.Ar*(dcp_dlamb*dlamb_dvm(x,i)*vri_eq(x,i)^3 + 3*cpi_eq(x,i)*ws_ts(x,i)*vri_eq(x,i)^2)/(4*x(1)*B.l); % dv_m
-e6 = @(x,i) Ae.rho*Ae.Ar*(dcp_dlamb*dlamb_dvt(x,i)*vri_eq(x,i)^3 + 3*cpi_eq(x,i)*vri_eq(x,i)^2)/(4*x(1)*B.l); % dv_t
-e7 = @(x,i) Ae.rho*Ae.Ar*(dcp_dlamb*dlamb_dxdott(x,i)*vri_eq(x,i)^3 - 3*cpi_eq(x,i)*vri_eq(x,i)^2)/(4*x(1)*B.l); % dxdot_t
-e8 = @(x,i) Ae.rho*Ae.Ar*(dcp_dlamb*dlamb_dxdotbi(x,i)*vri_eq(x,i)^3 - 3*cpi_eq(x,i)*vri_eq(x,i)^2)/(4*x(1)*B.l); % dxdot_bi
-e9 = @(x,i) Ae.rho*Ae.Ar*vri_eq(x,i)^3*(dcp_dlamb*dlamb_dwr(x,i)-cpi_eq(x,i)/x(1))/(4*x(1)*B.l); % dw_r
-e10 = @(x,i) Ae.rho*Ae.Ar*(3*cpi_eq(x,i)*dvri_dazim(x,i)*vri_eq(x,i)^2 + dcp_dlamb*dlamb_dazim(x,i)*vri_eq(x,i)^3)/(4*x(1)*B.l); % dazim
-e11 = @(x,i) Ae.rho*Ae.Ar*vri_eq(x,i)^3*dcp_dpitch/(4*x(1)*B.l); % dpitch
+e5 = @(x,i) Ae.rho*Ae.Ar*(dCp_dLambda_eq(x,i)*dlamb_dvm(x,i)*vri_eq(x,i)^3 + 3*cpi_eq(x,i)*ws_ts(x,i)*vri_eq(x,i)^2)/(4*x(1)*B.l); % dv_m
+e6 = @(x,i) Ae.rho*Ae.Ar*(dCp_dLambda_eq(x,i)*dlamb_dvt(x,i)*vri_eq(x,i)^3 + 3*cpi_eq(x,i)*vri_eq(x,i)^2)/(4*x(1)*B.l); % dv_t
+e7 = @(x,i) Ae.rho*Ae.Ar*(dCp_dLambda_eq(x,i)*dlamb_dxdott(x,i)*vri_eq(x,i)^3 - 3*cpi_eq(x,i)*vri_eq(x,i)^2)/(4*x(1)*B.l); % dxdot_t
+e8 = @(x,i) Ae.rho*Ae.Ar*(dCp_dLambda_eq(x,i)*dlamb_dxdotbi(x,i)*vri_eq(x,i)^3 - 3*cpi_eq(x,i)*vri_eq(x,i)^2)/(4*x(1)*B.l); % dxdot_bi
+e9 = @(x,i) Ae.rho*Ae.Ar*vri_eq(x,i)^3*(dCp_dLambda_eq(x,i)*dlamb_dwr(x,i)-cpi_eq(x,i)/x(1))/(4*x(1)*B.l); % dw_r
+e10 = @(x,i) Ae.rho*Ae.Ar*(3*cpi_eq(x,i)*dvri_dazim(x,i)*vri_eq(x,i)^2 + dCp_dLambda_eq(x,i)*dlamb_dazim(x,i)*vri_eq(x,i)^3)/(4*x(1)*B.l); % dazim
+e11 = @(x,i) Ae.rho*Ae.Ar*vri_eq(x,i)^3*dCp_dTheta_eq(x,i)/(4*x(1)*B.l); % dpitch
 
 % Theta_dot
-f1 = 2*Ac.omega*Ac.xi;
-f2 = Ac.omega;
+f1 = Ac.omega^2;
+f2 = 2*Ac.omega*Ac.xi;
 
 % Tg
 g1 = 1/Ac.tau;
@@ -81,5 +106,5 @@ p1 = B.kx*2*B.l/3;
 p2 = B.ky*2*B.l/3;
 
 % Pe
-q1 = @(x) D.eta*x(24);
-q2 = @(x) D.eta*x(1);
+q1 = @(x) D.eta*x(1);
+q2 = @(x) D.eta*x(24);
