f397106f02e1849692aaf57e94956998ea01a1b6
Thomas Haugaard <thomashaugaard_23@hotmail.com>
2022-05-12 13:41:30 +0200

    true_plots delete comments
    Deleted unused stuff

--- Main/functions/cp_ct.m
+++
@@ -1,5 +0,0 @@
-function res = cp_ct(la,be,cl,lambdaVec,pitchVec)
-[~,i_la] = min(abs(lambdaVec-abs(la)));
-[~,i_be] = min(abs(pitchVec-be));
-res = cl(i_la,i_be);
-end
--- Main/functions/cp_max.m
+++
@@ -1,6 +0,0 @@
-function [la,res] = cp_max(be,cl,lambdaVec,pitchVec)
-[~,i_be] = min(abs(pitchVec-be));
-l_c = cl(:,i_be);
-[res,i_la] = max(l_c);
-la = lambdaVec(i_la);
-end
