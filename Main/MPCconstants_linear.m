0bb8759a9d955881945eba4e0e7a3fd4d7871975
oierajenjo <oierajenjo@opendeusto.es>
2022-05-13 14:41:08 +0200

    Possibly bad math

--- Main/MPCconstants_linear.m
+++ Main/MPCconstants_linear.m
@@ -58,30 +58,32 @@ c3 = B.cy/To.m;
 c4 = (B.B*c2 + To.k/To.m);
 c5 = (B.B*c3 + To.c/To.m);
 
+
+%% CONTINUE HERE
 % xbi_dot
 d1 = B.kx/B.m; % dx_bi
-d2 = @(x,i) Ae.rho*Ae.Ar*(dCt_dLamb(x,i)*r7(x,i)*vri_eq(x,i)^2 - 2*cti_eq(x,i)*vri_eq(x,i))/(6*B.m) - B.cx/B.m; % dxdot_bi
+d2 = @(x,i) Ae.rho*Ae.Ar*(dCt_dLamb(x,i)*r7(x,i)*vri_eq(x,i)^2 - 2*cti_eq(x,i)*vri_eq(x,i))/(2*B.m) - B.cx/B.m; % dxdot_bi
 d3 = d1; % dx_t
-d4 = @(x,i) Ae.rho*Ae.Ar*(dCt_dLamb(x,i)*r6(x,i)*vri_eq(x,i)^2 - 2*cti_eq(x,i)*vri_eq(x,i))/(6*B.m) + B.cx/B.m; % dxdot_t
-d5 = @(x,i) Ae.rho*Ae.Ar*(dCt_dLamb(x,i)*r3(x,i)*vri_eq(x,i)^2 + 2*cti_eq(x,i)*ws_ts(x,i)*vri_eq(x,i))/(6*B.m); % dv_m
-d6 = @(x,i) Ae.rho*Ae.Ar*(dCt_dLamb(x,i)*r5(x,i)*vri_eq(x,i)^2 + 2*cti_eq(x,i)*vri_eq(x,i))/(6*B.m); % dv_t
-d7 = @(x,i) Ae.rho*Ae.Ar*vri_eq(x,i)^2*dCt_dTheta(x,i)/(6*B.m); % dpitch
-d8 = @(x,i) Ae.rho*Ae.Ar*vri_eq(x,i)^2*dCt_dLamb(x,i)*r2(x,i)/(6*B.m); % dydot_bi
-d9 = @(x,i) Ae.rho*Ae.Ar*(2*cti_eq(x,i)*dvri_dazim(x,i)*vri_eq(x,i) + dCt_dLamb(x,i)*r4(x,i)*vri_eq(x,i)^2)/(6*B.m); % dazim
-d10 = @(x,i) Ae.rho*Ae.Ar*vri_eq(x,i)^2*dCt_dLamb(x,i)*r1(x,i)/(6*B.m); % dw_r
+d4 = @(x,i) Ae.rho*Ae.Ar*(dCt_dLamb(x,i)*r6(x,i)*vri_eq(x,i)^2 - 2*cti_eq(x,i)*vri_eq(x,i))/(2*B.m) + B.cx/B.m; % dxdot_t
+d5 = @(x,i) Ae.rho*Ae.Ar*(dCt_dLamb(x,i)*r3(x,i)*vri_eq(x,i)^2 + 2*cti_eq(x,i)*ws_ts(x,i)*vri_eq(x,i))/(2*B.m); % dv_m
+d6 = @(x,i) Ae.rho*Ae.Ar*(dCt_dLamb(x,i)*r5(x,i)*vri_eq(x,i)^2 + 2*cti_eq(x,i)*vri_eq(x,i))/(2*B.m); % dv_t
+d7 = @(x,i) Ae.rho*Ae.Ar*vri_eq(x,i)^2*dCt_dTheta(x,i)/(2*B.m); % dpitch
+d8 = @(x,i) Ae.rho*Ae.Ar*vri_eq(x,i)^2*dCt_dLamb(x,i)*r2(x,i)/(2*B.m); % dydot_bi
+d9 = @(x,i) Ae.rho*Ae.Ar*(2*cti_eq(x,i)*dvri_dazim(x,i)*vri_eq(x,i) + dCt_dLamb(x,i)*r4(x,i)*vri_eq(x,i)^2)/(2*B.m); % dazim
+d10 = @(x,i) Ae.rho*Ae.Ar*vri_eq(x,i)^2*dCt_dLamb(x,i)*r1(x,i)/(2*B.m); % dw_r
 
 % ybi_dot
 e1 = B.ky/B.m; % dy_bi
 e2 = @(x,i) Ae.rho*Ae.Ar*vri_eq(x,i)^3*(dCp_dLamb(x,i)*r2(x,i))/(4*B.m*x(1)*B.l) + B.cy/B.m; % dydot_bi
 e3 = e1; % dy_t
 e4 = B.cy/B.m; % dydot_t
-e5 = @(x,i) Ae.rho*Ae.Ar*(dCp_dLamb(x,i)*r3(x,i)*vri_eq(x,i)^3 + 3*cpi_eq(x,i)*ws_ts(x,i)*vri_eq(x,i)^2)/(4*B.m*x(1)*B.l); % dv_m
-e6 = @(x,i) Ae.rho*Ae.Ar*(dCp_dLamb(x,i)*r5(x,i)*vri_eq(x,i)^3 + 3*cpi_eq(x,i)*vri_eq(x,i)^2)/(4*B.m*x(1)*B.l); % dv_t
-e7 = @(x,i) Ae.rho*Ae.Ar*(dCp_dLamb(x,i)*r6(x,i)*vri_eq(x,i)^3 - 3*cpi_eq(x,i)*vri_eq(x,i)^2)/(4*B.m*x(1)*B.l); % dxdot_t
-e8 = @(x,i) Ae.rho*Ae.Ar*(dCp_dLamb(x,i)*r7(x,i)*vri_eq(x,i)^3 - 3*cpi_eq(x,i)*vri_eq(x,i)^2)/(4*B.m*x(1)*B.l); % dxdot_bi
-e9 = @(x,i) Ae.rho*Ae.Ar*vri_eq(x,i)^3*(dCp_dLamb(x,i)*r1(x,i)-cpi_eq(x,i)/x(1))/(4*B.m*x(1)*B.l); % dw_r
-e10 = @(x,i) Ae.rho*Ae.Ar*(3*cpi_eq(x,i)*dvri_dazim(x,i)*vri_eq(x,i)^2 + dCp_dLamb(x,i)*r4(x,i)*vri_eq(x,i)^3)/(4*B.m*x(1)*B.l); % dazim
-e11 = @(x,i) Ae.rho*Ae.Ar*vri_eq(x,i)^3*dCp_dTheta(x,i)/(4*B.m*x(1)*B.l); % dpitch
+e5 = @(x,i) 3*Ae.rho*Ae.Ar*(dCp_dLamb(x,i)*r3(x,i)*vri_eq(x,i)^3 + 3*cpi_eq(x,i)*ws_ts(x,i)*vri_eq(x,i)^2)/(4*B.m*x(1)*B.l); % dv_m
+e6 = @(x,i) 3*Ae.rho*Ae.Ar*(dCp_dLamb(x,i)*r5(x,i)*vri_eq(x,i)^3 + 3*cpi_eq(x,i)*vri_eq(x,i)^2)/(4*B.m*x(1)*B.l); % dv_t
+e7 = @(x,i) 3*Ae.rho*Ae.Ar*(dCp_dLamb(x,i)*r6(x,i)*vri_eq(x,i)^3 - 3*cpi_eq(x,i)*vri_eq(x,i)^2)/(4*B.m*x(1)*B.l); % dxdot_t
+e8 = @(x,i) 3*Ae.rho*Ae.Ar*(dCp_dLamb(x,i)*r7(x,i)*vri_eq(x,i)^3 - 3*cpi_eq(x,i)*vri_eq(x,i)^2)/(4*B.m*x(1)*B.l); % dxdot_bi
+e9 = @(x,i) 3*Ae.rho*Ae.Ar*vri_eq(x,i)^3*(dCp_dLamb(x,i)*r1(x,i)-cpi_eq(x,i)/x(1))/(4*B.m*x(1)*B.l); % dw_r
+e10 = @(x,i) 3*Ae.rho*Ae.Ar*(3*cpi_eq(x,i)*dvri_dazim(x,i)*vri_eq(x,i)^2 + dCp_dLamb(x,i)*r4(x,i)*vri_eq(x,i)^3)/(4*B.m*x(1)*B.l); % dazim
+e11 = @(x,i) 3*Ae.rho*Ae.Ar*vri_eq(x,i)^3*dCp_dTheta(x,i)/(4*B.m*x(1)*B.l); % dpitch
 
 % Thetai_dot
 f1 = Ac.omega^2;
